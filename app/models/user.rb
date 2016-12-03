b4d4275674f4348c335bd631d4462931341365bd
Gaurav Sharma <gaurav2728@gmail.com>
2016-12-04 00:17:57 +0530

    add full_name, terms_of service in registration form
    * terms_of_service is not working fine now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :full_name, presence: true
+
+  # http://edgeguides.rubyonrails.org/active_record_validations.html#acceptance
+  # currently is not working, need to implement properly
+  validates :terms_of_service, acceptance: { message: 'must be accepted' }
 end
